cc285de8fc738cd4bae080a28476d074dde37b96
iqbalkang225 <iqbal.kang.cohort225@gmail.com>
2024-06-23 04:14:29 -0700

    updated package

--- src/apis/movie.js
+++ src/apis/movie.js
@@ -1,171 +1,171 @@
-import customFetch from '../utils/axios'
+import customFetch from '../utils/axios';
 
 const postTrailer = async (trailer, setUploadProgress) => {
   try {
     const { data } = await customFetch.post(`/movie/upload-trailer`, trailer, {
       onUploadProgress: ({ loaded, total }) => {
-        setUploadProgress(Math.floor((loaded / total) * 100))
+        setUploadProgress(Math.floor((loaded / total) * 100));
       },
-    })
-    return { data }
+    });
+    return { data };
   } catch (error) {
-    const { response } = error
-    if (response?.data) return { error: response.data }
-    return { error }
+    const { response } = error;
+    if (response?.data) return { error: response.data };
+    return { error };
   }
-}
+};
 
-const postMovie = async movieInfo => {
+const postMovie = async (movieInfo) => {
   try {
-    const { data } = await customFetch.post(`/movie`, movieInfo)
-    return { data }
+    const { data } = await customFetch.post(`/movie`, movieInfo);
+    return { data };
   } catch (error) {
-    console.log(error)
-    const { response } = error
-    if (response?.data) return { error: response.data }
-    return { error }
+    console.log(error);
+    const { response } = error;
+    if (response?.data) return { error: response.data };
+    return { error };
   }
-}
+};
 
 const updateMovie = async (id, movieInfo) => {
-  console.log(id)
+  console.log(id);
   try {
-    const { data } = await customFetch.patch(`/movie/${id}`, movieInfo)
-    return { data }
+    const { data } = await customFetch.patch(`/movie/${id}`, movieInfo);
+    return { data };
   } catch (error) {
-    console.log(error)
-    const { response } = error
-    if (response?.data) return { error: response.data.message }
-    return { error }
+    console.log(error);
+    const { response } = error;
+    if (response?.data) return { error: response.data.message };
+    return { error };
   }
-}
+};
 
 const getMovies = async (page, limit) => {
   try {
-    const { data } = await customFetch.get(`/movie?page=${page}&limit=${limit}`)
-    return { data }
+    const { data } = await customFetch.get(`/movie?page=${page}&limit=${limit}`);
+    return { data };
   } catch (error) {
-    console.log(error)
-    const { response } = error
-    if (response?.data) return { error: response.data }
-    return { error }
+    console.log(error);
+    const { response } = error;
+    if (response?.data) return { error: response.data };
+    return { error };
   }
-}
+};
 
-const getMovie = async id => {
+const getMovie = async (id) => {
   try {
-    const { data } = await customFetch.get(`/movie/${id}`)
-    return { data }
+    const { data } = await customFetch.get(`/movie/${id}`);
+    return { data };
   } catch (error) {
-    console.log(error)
-    const { response } = error
-    if (response?.data) return { error: response.data }
-    return { error }
+    console.log(error);
+    const { response } = error;
+    if (response?.data) return { error: response.data };
+    return { error };
   }
-}
+};
 
-const searchMovie = async title => {
+const searchMovie = async (title) => {
   try {
-    const { data } = await customFetch.get(`/movie/search?title=${title}`)
-    return { data }
+    const { data } = await customFetch.get(`/movie/search?title=${title}`);
+    return { data };
   } catch (error) {
-    console.log(error)
-    const { response } = error
-    if (response?.data) return { error: response.data }
-    return { error }
+    console.log(error);
+    const { response } = error;
+    if (response?.data) return { error: response.data };
+    return { error };
   }
-}
+};
 
-const deleteMovie = async id => {
+const deleteMovie = async (id) => {
   try {
-    const { data } = await customFetch.delete(`/movie/${id}`)
-    console.log(data)
-    return { data }
+    const { data } = await customFetch.delete(`/movie/${id}`);
+    console.log(data);
+    return { data };
   } catch (error) {
-    console.log(error)
-    const { response } = error
-    if (response?.data) return { error: response.data }
-    return { error }
+    console.log(error);
+    const { response } = error;
+    if (response?.data) return { error: response.data };
+    return { error };
   }
-}
+};
 
-const getTopRated = async type => {
-  let url = 'movie/top-rated'
-  if (type) url = `${url}?type=${type}`
+const getTopRated = async (type) => {
+  let url = 'movie/top-rated';
+  if (type) url = `${url}?type=${type}`;
   try {
-    const { data } = await customFetch.get(url)
+    const { data } = await customFetch.get(url);
     // console.log(data)
-    return { data }
+    return { data };
   } catch (error) {
-    console.log(error)
-    const { response } = error
-    if (response?.data) return { error: response.data }
-    return { error }
+    console.log(error);
+    const { response } = error;
+    if (response?.data) return { error: response.data };
+    return { error };
   }
-}
+};
 
-const getLatestMovies = async limit => {
-  let url = `movie/lastest-uploads`
-  if (limit) url = url + `?limit=${limit}`
+const getLatestMovies = async (limit) => {
+  let url = `movie/lastest-uploads`;
+  if (limit) url = url + `?limit=${limit}`;
 
   try {
-    const { data } = await customFetch.get(url)
-    return { data }
+    const { data } = await customFetch.get(url);
+    return { data };
   } catch (error) {
-    console.log(error)
-    const { response } = error
-    if (response?.data) return { error: response.data }
-    return { error }
+    console.log(error);
+    const { response } = error;
+    if (response?.data) return { error: response.data };
+    return { error };
   }
-}
+};
 
-const getSingleMovie = async id => {
+const getSingleMovie = async (id) => {
   try {
-    const { data } = await customFetch.get(`movie/single-movie/${id}`)
-    return { data }
+    const { data } = await customFetch.get(`movie/single-movie/${id}`);
+    return { data };
   } catch (error) {
-    console.log(error)
-    const { response } = error
-    if (response?.data) return { error: response.data }
-    return { error }
+    console.log(error);
+    const { response } = error;
+    if (response?.data) return { error: response.data };
+    return { error };
   }
-}
+};
 
-const getRelatedMovies = async id => {
+const getRelatedMovies = async (id) => {
   try {
-    const { data } = await customFetch.get(`movie/related-movies/${id}`)
-    return { data }
+    const { data } = await customFetch.get(`movie/related-movies/${id}`);
+    return { data };
   } catch (error) {
-    console.log(error)
-    const { response } = error
-    if (response?.data) return { error: response.data }
-    return { error }
+    console.log(error);
+    const { response } = error;
+    if (response?.data) return { error: response.data };
+    return { error };
   }
-}
+};
 
-const searchMovieUser = async title => {
+const searchMovieUser = async (title) => {
   try {
-    const { data } = await customFetch.get(`/movie/movie-search?title=${title}`)
-    return { data }
+    const { data } = await customFetch.get(`/movie/movie-search?title=${title}`);
+    return { data };
   } catch (error) {
-    console.log(error)
-    const { response } = error
-    if (response?.data) return { error: response.data }
-    return { error }
+    console.log(error);
+    const { response } = error;
+    if (response?.data) return { error: response.data };
+    return { error };
   }
-}
+};
 
 const getAllMovies = async (page, limit) => {
   try {
-    const { data } = await customFetch.get(`/movie/all-movies?page=${page}&limit=${limit}`)
-    return { data }
+    const { data } = await customFetch.get(`/movie/all-movies?page=${page}&limit=${limit}`);
+    return { data };
   } catch (error) {
-    console.log(error)
-    const { response } = error
-    if (response?.data) return { error: response.data }
-    return { error }
+    console.log(error);
+    const { response } = error;
+    if (response?.data) return { error: response.data };
+    return { error };
   }
-}
+};
 
 export {
   postTrailer,
@@ -181,4 +181,4 @@ export {
   getRelatedMovies,
   searchMovieUser,
   getAllMovies,
-}
+};
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const customFetch = axios.create({
-  // baseURL: 'http://3.101.140.214/api/v1',
   // baseURL: 'http://127.0.0.1:8000/api/v1',
-  baseURL: 'http://52.53.253.100/api/v1/',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export default customFetch;
